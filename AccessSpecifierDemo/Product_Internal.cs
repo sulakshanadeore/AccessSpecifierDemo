ffb66093cfaa3565cfd856a618dc45b40a607fe5
deoresulakshana <deoresulakshana@gmail.com>
2024-11-08 10:15:38 +0530

    Add project files.

---
+++ AccessSpecifierDemo/Product_Internal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSpecifierDemo
+{
+    internal class ElectronicsProducts
+    {
+        internal int Watt { get; set; }
+        protected int EP_ID { get; set; }
+
+        public int ShowDetails()
+        { 
+        Product_Internal internalporoduct=new Product_Internal();
+            return internalporoduct.Price;
+        }
+        
+
+        protected internal int show_ElectronicProductID()
+        {
+            return EP_ID;
+        }
+
+
+    }
+
+    internal class Mobiles : ElectronicsProducts
+    {
+        public int ElectronicProducID 
+        {
+            get { return base.EP_ID; }
+            set { base.EP_ID=value; }
+        }
+
+
+        public int MyProperty { get {
+                return base.ShowDetails();
+            }
+        }
+
+
+
+    }
+
+
+    internal class Product_Internal
+    {
+        private string productname;
+
+        public string ProductName
+        {
+            get { return productname; }
+            set { productname = value; }
+        }
+        public int Productid { get; set; }
+
+        protected internal int Price { get; set; }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ AccessSpecifierDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSpecifierDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product_Internal internalobj =new Product_Internal();
+            //ElectronicsProducts ep = new ElectronicsProducts();
+            
+            Mobiles m = new Mobiles();
+            Console.WriteLine("Enter electronic Product ID");
+            m.ElectronicProducID=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(m.ElectronicProducID);
+            Console.Read();
+        }
+    }
+}
